7cdb35e1b7ad17e436562df367e01e517dcb5368
zzoaJW <i7can7i@gmail.com>
2023-01-07 20:37:02 +0900

    Refactoring : Splash Handler

--- app/src/main/java/com/z0o0a/lid/Splash.kt
+++ app/src/main/java/com/z0o0a/lid/Splash.kt
@@ -3,6 +3,7 @@ package com.z0o0a.lid
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.z0o0a.lid.databinding.SplashBinding
 
@@ -15,19 +16,11 @@ class Splash : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        },DURATION)
-
-    }
-    companion object {
-        private const val DURATION : Long = 2000
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
+        }, 2000)
     }
 }
