cf66d9c4369e9b629a6526a26cf972526e7889a6
z(0o0a) <i7can7i@gmail.com>
2023-03-04 23:56:09 +0900

    Posting MVVM 리팩토링 : 코드 간소화 리팩토링 (기존: 모든 속성을 따로 livedata 설정 / 변경: 객체단위로 livedata 설정)

--- app/src/main/java/com/z0o0a/lid/viewmodel/DrinkPostingVM.kt
+++ app/src/main/java/com/z0o0a/lid/viewmodel/DrinkPostingVM.kt
@@ -6,54 +6,59 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.room.ColumnInfo
+import com.z0o0a.lid.Drink
+import com.z0o0a.lid.DrinkBeer
+import com.z0o0a.lid.DrinkWhiskey
+import com.z0o0a.lid.DrinkWine
 
 class DrinkPostingVM(application: Application) : AndroidViewModel(application) {
-    var drinkImg = MutableLiveData<Bitmap>() // 이미지
-    var drinkEngName = MutableLiveData<String>() // 영어 이름
-    var drinkKrName = MutableLiveData<String>() // 한글 이름
-    var drinkType = ""
-    // var typeId = MutableLiveData<Long>() // 위스키/와인/맥주테이블에서의 고유번호 (위/와/맥이 아닌 경우 0 저장)
+    // [ 이름 페이지 ]
+    // (1) 영어 이름 (2) 원하는 언어 이름 (3) 술 종류
+    // [ 촬영or이미지 페이지 ]
+    // (1) 촬영 화질 높이기 (2) 이미지 선택
+    // (3) 이미지 선택 안했을때 null로 저장하되 나중에 NullPoint예외 안나오게하는 방법 찾기 (예외 발생 안하고 기본 이미지 불러오는 방법)
+    // [ 나머지 정보 페이지 ]
+    // 술 종류마다 다른 정보 입력 받기
 
-    var drinkOverallStr = MutableLiveData<String>() // 총평 줄글로 쓴거
-    var drinkRating = MutableLiveData<Float>() // 별점
-    var drinkRegion = MutableLiveData<String>() // 생산 국가/지역
-    var drinkPrice = MutableLiveData<String>() // 가격
+    val drink = MutableLiveData<Drink>()
+    val drinkWhiskey = MutableLiveData<DrinkWhiskey>()
+    val drinkWine = MutableLiveData<DrinkWine>()
+    val drinkBeer = MutableLiveData<DrinkBeer>()
 
-    var drinkKeepDate = MutableLiveData<String>() // 개봉 일자
-    var drinkPlace = MutableLiveData<String>() // 시음 장소
-    var drinkPostingDate = MutableLiveData<String>() // 작성 일자
-
-    var wiskey = MutableLiveData<String>()
 
     init {
-        // drinkImg.value =
-        drinkEngName.value = ""
-        drinkEngName.value = ""
-        drinkKrName.value = ""
+        drink.value = Drink(0,null, "", "", "", 0L, "",
+                             0f, "", "", "", "", "")
+        drinkWhiskey.value = DrinkWhiskey(0, false, "-", "", arrayListOf(), arrayListOf(),
+                                   3, 3, 3, arrayListOf())
+        drinkWine.value = DrinkWine(0, false, "-", 0, 0, 0, false,
+                                     3, "", "", arrayListOf(), 3, 3, 3, 3)
+        drinkBeer.value = DrinkBeer(0, false, "-", "", 3, "", 3, 3,
+                                      3, 3, 3, "", 3, 3, 3,
+                                      3, 3, 3, 3)
 
-        drinkOverallStr.value = ""
-        drinkRating.value = 0.0f
-        drinkRegion.value = ""
-        drinkPrice.value = ""
-
-        drinkKeepDate.value = ""
-        drinkPlace.value = ""
-        drinkPostingDate.value = ""
     }
 
-    fun setDrinkType(dType : Int){
+    fun setDrinkType(drinkType : Int){
         // 위스키 0 / 와인 1 / 맥주 2 / 기타 3
-        when (dType) {
-            0 -> drinkType = "위스키"
-            1 -> drinkType = "와인"
-            2 -> drinkType = "맥주"
-            3 -> {
-                drinkType = "기타"
-            }
+        when (drinkType) {
+            0 -> drink.value!!.drinkType = "위스키"
+            1 -> drink.value!!.drinkType = "와인"
+            2 -> drink.value!!.drinkType = "맥주"
+            3 -> drink.value!!.drinkType = "기타"
         }
     }
 
     fun checkNameType(){
 
+    }
+
+    // 개봉일 선택 날짜 visible
+    fun setKeepDateVisible(checked : Boolean){
+        if (checked) {
+            // 개봉일 보이게
+        }else {
+            // 안보이게
+        }
     }
 }
