e58063a02cb8ecd77aac9fcb4d4be898f553fcae
z(0o0a) <i7can7i@gmail.com>
2023-03-08 17:18:07 +0900

    Posting MVVM 리팩토링 : NavHost 생성

---
+++ app/src/main/java/com/z0o0a/lid/view/DrinkPostingHost.kt
@@ -0,0 +1,13 @@
+package com.z0o0a.lid.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.z0o0a.lid.R
+
+class DrinkPostingHost : AppCompatActivity(R.layout.drink_posting_host) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
