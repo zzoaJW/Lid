3946583ee1e6767b8da47a14815a6fe6f3febb69
z(0o0a) <i7can7i@gmail.com>
2023-03-11 04:56:09 +0900

    Posting MVVM 리팩토링 : wine posting 1차 리팩토링

--- app/src/main/java/com/z0o0a/lid/DrinkPostingDetailWine.kt
+++ app/src/main/java/com/z0o0a/lid/DrinkPostingDetailWine.kt
@@ -5,68 +5,48 @@ import android.app.DatePickerDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.chip.Chip
+import com.z0o0a.lid.databinding.DrinkPostingDetailBinding
 import com.z0o0a.lid.databinding.DrinkPostingDetailWineBinding
 import com.z0o0a.lid.model.PostingDrinkSingleton
 import com.z0o0a.lid.repository.DrinkDatabase
+import com.z0o0a.lid.viewmodel.DrinkPostingVM
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DrinkPostingDetailWine : AppCompatActivity() {
+class DrinkPostingDetailWine : Fragment()  {
     private lateinit var binding: DrinkPostingDetailWineBinding
-
-    var wiShort = false
-
-    var drinkImg : Bitmap? = null
-    var drinkEngName = ""
-    var drinkKrName = ""
-    var drinkType = ""
-
-    var wiId : Long = 0
-    var wiType = "-" // 소분류 (일단 다 -로 저장)
-
-    var wiClarity = 3
-    var wiBrightness = 3
-    var wiTears = 3
-
-    var wiRimVariation = false
-    var wiRimArea = 3
-    var wiRimColor = "" // 일단 디폴드 값으로 저장
-    var wiCoreColor = "" // 일단 디폴드 값으로 저장
+    private val vm: DrinkPostingVM by activityViewModels()
 
     val wiNoses : MutableList<String> = mutableListOf()
-    var wiSweet = 3
-    var wiAcidity = 3
-    var wiBody = 3
-    var wiTannin = 3
 
-    var wiRating = 0f
-    var wiOverallStr = ""
-    var wiRegion = ""
-    var wiPrice = ""
-
-    var wiPlace = ""
-    var wiKeepDate = ""
-    var wiPostingDate = ""
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DrinkPostingDetailWineBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        binding.vm = vm
+        binding.lifecycleOwner = requireActivity()
+        return binding.root
+    }
 
-        getSingletonValues()
-        setSingletonValuesToUI()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setDrinkImg()
 
         binding.btnWineBack.setOnClickListener {
-            finish()
+            findNavController().navigate(R.id.drinkPostingMedia)
         }
 
         binding.btnWineCancel.setOnClickListener {
@@ -78,13 +58,7 @@ class DrinkPostingDetailWine : AppCompatActivity() {
         }
 
         binding.switchPostWiRimVariation.setOnClickListener{
-            if (binding.postWiRimLayout.isVisible){
-                wiRimVariation = false
-                binding.postWiRimLayout.visibility = View.GONE
-            }else{
-                wiRimVariation = true
-                binding.postWiRimLayout.visibility = View.VISIBLE
-            }
+            setRimVariationVisible()
         }
 
         binding.postWiNoseChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
@@ -97,14 +71,7 @@ class DrinkPostingDetailWine : AppCompatActivity() {
         }
 
         binding.switchWineKeep.setOnClickListener {
-            if (binding.txtWineKeep.isVisible) {
-                binding.txtWineKeep.visibility = View.INVISIBLE
-                binding.wineKeepDate.visibility = View.VISIBLE
-            } else {
-                binding.wineKeepDate.text = "개봉일 선택"
-                binding.wineKeepDate.visibility = View.INVISIBLE
-                binding.txtWineKeep.visibility = View.VISIBLE
-            }
+            setDatePickerVisible()
         }
 
         binding.wineKeepDate.setOnClickListener {
@@ -112,25 +79,100 @@ class DrinkPostingDetailWine : AppCompatActivity() {
         }
 
         binding.btnWinePosting.setOnClickListener {
-            getValues()
+            try {
+                saveDrink()
+                Toast.makeText(requireContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show()
 
-            saveWine()
-            saveDrink()
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
 
-            Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
-
-            intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+                activity?.finish()
+            }catch (e:Exception){
+                Toast.makeText(requireContext(), "저장에 실패하였습니다. 관리자에 문의해주세요.", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
 
+
+    private fun setDrinkImg(){
+        val drinkImg = vm.drink.value!!.drinkImg
+
+        if(drinkImg == null){
+            binding.postingWineImg.setImageBitmap(BitmapFactory.decodeResource(resources, R.drawable.bottle))
+        }else{
+            binding.postingWineImg.setImageBitmap(drinkImg)
+        }
+    }
+
+    private fun hideDetail(){
+        if (binding.wineDetailLayout.visibility == View.VISIBLE) {
+            vm.drinkWine.value!!.wiShort = true
+            binding.wineDetailLayout.visibility = View.GONE
+            binding.btnWineDetail.setTextColor(Color.parseColor("#E0F14E"))
+        } else{
+            vm.drinkWine.value!!.wiShort = false
+            binding.wineDetailLayout.visibility = View.VISIBLE
+            binding.btnWineDetail.setTextColor(Color.parseColor("#CDCDCD"))
+        }
+    }
+
+    private fun setRimVariationVisible() {
+        if (binding.postWiRimLayout.isVisible){
+            vm.drinkWine.value!!.wiRimVariation = false
+            binding.postWiRimLayout.visibility = View.GONE
+        }else{
+            vm.drinkWine.value!!.wiRimVariation = true
+            binding.postWiRimLayout.visibility = View.VISIBLE
+        }
+    }
+
+    // TODO : data binding으로 해결하기
+    private fun setWiNose(){
+        vm.drinkWine.value!!.wiNose = wiNoses
+    }
+
+    private fun setDatePickerVisible(){
+        if (binding.txtWineKeep.isVisible) {
+            binding.txtWineKeep.visibility = View.INVISIBLE
+            binding.wineKeepDate.visibility = View.VISIBLE
+        } else {
+            binding.txtWineKeep.visibility = View.VISIBLE
+            binding.wineKeepDate.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun showKeepDateDialog(){
+        val cal = Calendar.getInstance()    //캘린더뷰 만들기
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            val dateString = "${year}.${month+1}.${dayOfMonth}"
+            binding.wineKeepDate.text = dateString
+        }
+        DatePickerDialog(requireContext(), dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+    }
+
+    private fun getCurrentDate(): String {
+        val now = System.currentTimeMillis()
+        val y = SimpleDateFormat("yyyy", Locale.KOREAN).format(now).toInt()
+        val m = SimpleDateFormat("MM", Locale.KOREAN).format(now).toInt()
+        val d = SimpleDateFormat("dd", Locale.KOREAN).format(now).toInt()
+
+        return "${y}.${m}.${d}"
+    }
+
+    private fun saveDrink() {
+        setWiNose()
+        vm.drink.value!!.drinkPostingDate = getCurrentDate()
+
+        vm.insertDrinkWine()
+        vm.insertDrink()
     }
 
     private fun cancelConfirm(){
-        AlertDialog.Builder(this)
+        AlertDialog.Builder(requireContext())
             .setTitle("작성을 취소하시겠습니까?")
             .setPositiveButton("네", object : DialogInterface.OnClickListener {
                 override fun onClick(dialog: DialogInterface, which: Int) {
-                    intent = Intent(applicationContext, MainActivity::class.java)
+                    val intent = Intent(requireContext(), MainActivity::class.java)
                     startActivity(intent)
                 }
             })
@@ -141,149 +183,5 @@ class DrinkPostingDetailWine : AppCompatActivity() {
             })
             .create()
             .show()
-    }
-
-    private fun hideDetail(){
-        if (binding.wineDetailLayout.visibility == View.VISIBLE) {
-            wiShort = true
-            binding.wineDetailLayout.visibility = View.GONE
-            binding.btnWineDetail.setTextColor(Color.parseColor("#E0F14E"))
-        } else{
-            wiShort = false
-            binding.wineDetailLayout.visibility = View.VISIBLE
-            binding.btnWineDetail.setTextColor(Color.parseColor("#CDCDCD"))
-        }
-    }
-
-    private fun showKeepDateDialog(){
-        var dateString = ""
-
-        val cal = Calendar.getInstance()    //캘린더뷰 만들기
-        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-            dateString = "${year}.${month+1}.${dayOfMonth}"
-            binding.wineKeepDate.text = dateString
-        }
-        DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(
-            Calendar.DAY_OF_MONTH)).show()
-    }
-
-    private fun getSingletonValues(){
-        val postingSingleton = PostingDrinkSingleton.getInstance(applicationContext)
-
-        drinkImg = postingSingleton?.drinkImg
-        drinkEngName = postingSingleton?.drinkEngName.toString()
-        drinkKrName = postingSingleton?.drinkKrName.toString()
-        drinkType = postingSingleton?.drinkType.toString()
-
-    }
-
-    private fun setSingletonValuesToUI(){
-        binding.postingWineImg.setImageBitmap(drinkImg)
-        binding.postingWineEngName.text = drinkEngName
-        binding.postingWineKrName.text = drinkKrName
-        binding.postingWineType.text = drinkType
-    }
-
-    private fun getValues(){
-        getAppearance()
-        getPalate()
-
-        // getRating
-        wiRating = binding.postWRatingBar.rating
-        // getOverallStr
-        wiOverallStr = binding.wiOverallStr.text.toString()
-
-        getInfo()
-    }
-
-    private fun getAppearance(){
-        wiClarity = binding.postWiClarity.progress
-        wiBrightness = binding.postWiBrightness.progress
-        wiTears  = binding.postWiTear.progress
-        wiRimArea = binding.postWiRimArea.progress
-    }
-
-    private fun getPalate(){
-        wiSweet = binding.postWiSweet.progress
-        wiAcidity = binding.postWiAcidity.progress
-        wiBody = binding.postWiBody.progress
-        wiTannin = binding.postWiTannin.progress
-    }
-
-    private fun getInfo(){
-        wiRegion = binding.wineRegion.text.toString()
-        wiPrice = binding.winePrice.text.toString()
-        wiPlace = binding.winePlace.text.toString()
-
-        wiKeepDate = binding.wineKeepDate.text.toString()
-        if (wiKeepDate == "개봉일 선택") {
-            wiKeepDate = "보관 안함"
-        }
-        wiPostingDate = getCurrentDate()
-    }
-
-    fun getCurrentDate(): String {
-        val now = System.currentTimeMillis()
-        val y = SimpleDateFormat("yyyy", Locale.KOREAN).format(now).toInt()
-        val m = SimpleDateFormat("MM", Locale.KOREAN).format(now).toInt()
-        val d = SimpleDateFormat("dd", Locale.KOREAN).format(now).toInt()
-
-        return "${y}.${m}.${d}"
-    }
-
-    private fun saveWine() {
-        val run : Runnable = Runnable {
-            var drinkWine = DrinkWine(0,
-                wiShort,
-                wiType,
-                wiClarity,
-                wiBrightness,
-                wiTears,
-                wiRimVariation,
-                wiRimArea,
-                wiRimColor,
-                wiCoreColor,
-                wiNoses,
-                wiSweet,
-                wiAcidity,
-                wiBody,
-                wiTannin
-            )
-
-            val db = DrinkDatabase.getInstance(applicationContext)
-            var returnWiId = db!!.drinkDao().insertDrinkWine(drinkWine)
-
-            wiId = returnWiId
-        }
-
-        val t = Thread(run)
-        t.start()
-
-        try {
-            t.join()
-        }catch (e : InterruptedException){
-            Log.d("와인 저장 실패","실패, 예외 발생")
-        }
-    }
-
-    private fun saveDrink() {
-        Thread(Runnable {
-            var newDrink = Drink(0,
-                drinkImg,
-                drinkEngName,
-                drinkKrName,
-                drinkType,
-                wiId,
-                wiOverallStr,
-                wiRating,
-                wiRegion,
-                wiPrice,
-                wiKeepDate,
-                wiPlace,
-                wiPostingDate)
-
-            val db = DrinkDatabase.getInstance(applicationContext)
-            db!!.drinkDao().insertDrink(newDrink)
-        }).start()
     }
 }
