6f31f2677c2031cbb207ac437d3a0a09b801d62f
z(0o0a) <i7can7i@gmail.com>
2023-03-11 16:50:09 +0900

    Posting MVVM 리팩토링 : 카메라 촬영 사진 고화질로 가져오기

--- app/src/main/java/com/z0o0a/lid/view/DrinkPostingMedia.kt
+++ app/src/main/java/com/z0o0a/lid/view/DrinkPostingMedia.kt
@@ -1,22 +1,37 @@
 package com.z0o0a.lid.view
 
 import android.Manifest
+import android.R.attr
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import com.z0o0a.lid.R
 import com.z0o0a.lid.databinding.DrinkPostingMediaBinding
 import com.z0o0a.lid.viewmodel.DrinkPostingVM
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 
 
 class DrinkPostingMedia : Fragment() {
     private lateinit var binding: DrinkPostingMediaBinding
     private val vm: DrinkPostingVM by activityViewModels()
+
+    var mCurrentPhotoPath = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DrinkPostingMediaBinding.inflate(layoutInflater)
@@ -28,15 +43,13 @@ class DrinkPostingMedia : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        // TODO (1) 권한 확인
         vm.getPermissionRequestResult().observe(viewLifecycleOwner) { result ->
             val cameraPermission = Manifest.permission.CAMERA
             val galleryPermission = Manifest.permission.READ_EXTERNAL_STORAGE
 
-            if (result[cameraPermission] != true) {
-                Toast.makeText(requireContext(), "카메라 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
-            }
+//            if (result[cameraPermission] != true) {
+//                Toast.makeText(requireContext(), "카메라 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+//            }
             if (result[galleryPermission] != true) {
                 Toast.makeText(requireContext(), "갤러리 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
             }
@@ -47,10 +60,7 @@ class DrinkPostingMedia : Fragment() {
 
 
         binding.btnGetCamera.setOnClickListener {
-            // TODO (2-1) 기본 카메라 촬영
-            // TODO (2-2) 촬영 화질 높이기
-            // TODO (2-3) 촬영한 사진 가져오기 [Repo -> (VM ->) View]
-            // TODO (2-4) 촬영한 사진 가져오면 imageView 바꾸기 [View]
+            getCameraPhoto()
         }
 
         binding.btnGetImg.setOnClickListener {
@@ -82,4 +92,69 @@ class DrinkPostingMedia : Fragment() {
         }
     }
 
+    private fun getCameraPhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(requireActivity().packageManager) != null){
+            var photoFile = requireActivity().cacheDir
+
+            val now = System.currentTimeMillis()
+            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(now)
+            val imgFileName = "Camera_" + timeStamp
+
+            try{
+                val tempImg = File.createTempFile(imgFileName, ".jpg", photoFile)
+                mCurrentPhotoPath = tempImg.absolutePath
+                photoFile = tempImg
+
+            }catch (e : IOException){
+                Toast.makeText(requireContext(), "입출력 예외", Toast.LENGTH_SHORT).show()
+            }
+
+            if(photoFile != null){
+                val aut = requireActivity().packageName + ".fileprovider"
+                val photoURI = FileProvider.getUriForFile(requireContext(), aut, photoFile)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(intent, 200)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 200){
+            try {
+                val file = File(mCurrentPhotoPath)
+                val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, Uri.fromFile(file))
+
+                if(bitmap != null){
+                    val ei = ExifInterface(mCurrentPhotoPath)
+                    val orientation: Int = ei.getAttributeInt(
+                        ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED
+                    )
+
+                    var rotatedBitmap: Bitmap? = null
+                    rotatedBitmap = when (orientation) {
+                        ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bitmap, 90f)
+                        ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bitmap, 180f)
+                        ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bitmap, 270f)
+                        else -> bitmap
+                    }
+
+                    binding.galleyPic.setImageBitmap(rotatedBitmap)
+                    vm.drink.value!!.drinkImg = rotatedBitmap
+                }
+            } catch (e: Exception) {
+                val expt = "[get cameraPhoto Error] Exception : ${e.message}, Cause : ${e.cause}"
+                Firebase.crashlytics.log(expt)
+            }
+
+        }
+    }
+
+    fun rotateImage(source: Bitmap, angle: Float): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
+    }
 }
